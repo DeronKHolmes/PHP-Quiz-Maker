2a4b4df46555c1fb8702fbbc5bd905f7218bc38e
Deron <DeronKHolmes@gmail.com>
2015-10-08 11:59:44 -0400

    Create database.php
    Edit this file accordingly to server needs.

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+	
+	// Connect to database
+	$db_host = 'localhost';
+	$db_name = 'quizzer';
+	$db_user = 'root';
+	$db_password = '';
+
+	//Create mysqli object
+	$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+	//Error Handler
+	if ($mysqli->connect_error) {
+		printf("Connect failed: %s\n", $mysqli->connect_error);
+		exit();
+	}
